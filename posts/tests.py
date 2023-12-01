1546f93f63cd2445e07072772d0736cb4cfd491c
WelshDan <>
2023-12-01 20:47:28 +0000

    Added post list view tests

--- posts/tests.py
+++ posts/tests.py
@@ -6,10 +6,23 @@ from rest_framework.test import APITestCase
 
 class PostListViewTests(APITestCase):
     def setUp(self):
-        User.objects.create_user(username='Sam', password='Monday123')
+        User.objects.create_user(username='sam', password='monday123')
 
     def test_can_list_posts(self):
-        adam = User.objects.get(username='Sam')
-        Post.objects.create(owner=Sam, title='a title')
+        sam = User.objects.get(username='sam')
+        Post.objects.create(owner=sam, title='a title')
         response = self.client.get('/posts/')
         self.assertEqual(response.status_code, status.HTTP_200_OK)
+        print(response.data)
+        print(len(response.data))
+
+    def test_logged_in_user_can_create_post(self):
+        self.client.login(username='sam', password='monday123')
+        response = self.client.post('/posts/', {'title': 'a title'})
+        count = Post.objects.count()
+        self.assertEqual(count, 1)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+    def test_user_not_logged_in_cant_create_post(self):
+        response = self.client.post('/posts/', {'title': 'a title'})
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
