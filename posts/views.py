18ca7e3c7215cadf0ba21b574278712414fe2b04
WelshDan <>
2023-12-01 14:00:27 +0000

    Added PostDetail view so it could get, edit and delete

--- posts/views.py
+++ posts/views.py
@@ -52,3 +52,22 @@ class PostDetail(APIView):
             post, context={'request': request}
         )
         return Response(serializer.data)
+
+    def put(self, request, pk):
+        post = self.get_object(pk)
+        serializer = PostSerializer(
+            post, data=request.data, context={'request': request}
+        )
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(
+            serializer.errors, status=status.HTTP_400_BAD_REQUEST
+        )
+
+    def delete(self, request, pk):
+        post = self.get_object(pk)
+        post.delete()
+        return Response(
+            status=status.HTTP_204_NO_CONTENT
+        )
