39751196451059ee6628803c8b2017d973fbab2f
WelshDan <>
2023-12-02 00:30:09 +0000

    Added CommentList and CommentDetail to views

--- comments/serializers.py
+++ comments/serializers.py
@@ -2,13 +2,13 @@ from rest_framework import serializers
 from .models import Comment
 
 
-class CommentSerializer(serializer.ModelSerializers):
+class CommentSerializer(serializers.ModelSerializer):
     owner = serializers.ReadOnlyField(source='owner.username')
     is_owner = serializers.SerializerMethodField()
     profile_id = serializers.ReadOnlyField(source='owner.profile_id')
     profile_image = serializers.ReadOnlyField(source='owner.profile.image.url')
 
-    def get_is_owned(self, obj):
+    def get_is_owner(self, obj):
         request = self.context['request']
         return request.user == obj.owner
 
@@ -18,3 +18,7 @@ class CommentSerializer(serializer.ModelSerializers):
             'id', 'owner', 'is_owner', 'profile_id', 'profile_image',
             'post', 'created_at', 'updated_at', 'content'
         ]
+
+
+class CommentDetailSerializer(CommentSerializer):
+    post = serializers.ReadOnlyField(source='post.id')
