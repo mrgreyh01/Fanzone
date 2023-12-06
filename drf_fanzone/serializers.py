55ba3e65b3aa80e43c2f4b0344b08957c42be1c8
WelshDan <>
2023-12-06 14:18:20 +0000

    intalled dj-database-url and psychopg2

--- drf_fanzone/serializers.py
+++ drf_fanzone/serializers.py
@@ -6,7 +6,6 @@ class CurrentUserSerializer(UserDetailsSerializer):
     profile_id = serializers.ReadOnlyField(source='profile.id')
     profile_image = serializers.ReadOnlyField(source='profile.image.url')
 
-
     class Meta(UserDetailsSerializer.Meta):
         fields = UserDetailsSerializer.Meta.fields + (
             'profile_id', 'profile_image'
