4fcfba775e816c368d319c9fa4803aac6df8dd07
WelshDan <>
2023-12-11 07:52:17 +0000

    CORRECTION: identing and text errors in drf/views file

--- drf_fanzone/views.py
+++ drf_fanzone/views.py
@@ -1,15 +1,17 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from .setting import (
-    JWT_AUTH_COOKIE, JWT_AUTH_REFRESH_COOKIE,JWT_AUTH_SAMESITE,
+from .settings import (
+    JWT_AUTH_COOKIE, JWT_AUTH_REFRESH_COOKIE, JWT_AUTH_SAMESITE,
     JWT_AUTH_SECURE,
 )
+
 
 @api_view()
 def root_route(request):
     return Response({
         "message": "Welcome to my drf called Fanzone"
     })
+
 
 # dj-rest-auth logout view fix
 @api_view(['POST'])
