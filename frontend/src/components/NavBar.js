bae4239d796a762a2c1d4b6aec7c8d798c34af2d
WelshDan <>
2023-12-13 22:37:28 +0000

    combined fanzone which is frontend with drf-fanzone which is the backend

---
+++ frontend/src/components/NavBar.js
@@ -0,0 +1,115 @@
+import React from "react";
+import { Navbar, Container, Nav } from "react-bootstrap";
+import logo from "../assets/logo.png";
+import styles from "../styles/NavBar.module.css";
+import { NavLink } from "react-router-dom";
+import { useCurrentUser, useSetCurrentUser } from "../contexts/CurrentUserContext";
+import Avatar from "./Avatar";
+import axios from "axios";
+import useClickOutsideToggle from "../hooks/useClickOutsideToggle";
+
+const NavBar = () => {
+    const currentUser = useCurrentUser();
+    const setCurrentUser = useSetCurrentUser();
+
+    const {expanded, setExpanded, ref} = useClickOutsideToggle();
+
+    const handleSignOut = async () => {
+        try {
+            await axios.post('dj-rest-auth/logout');
+            setCurrentUser(null);
+        } catch (err) {
+            console.log(err);
+        }
+    };
+
+    const addPostIcon = (
+        <NavLink
+            className={styles.NavLink}
+            activeClassName={styles.Active}
+            to="/posts/create"
+        >
+            <i className="fa-solid fa-plus"></i>Add post
+        </NavLink>
+    );
+    const loggedInIcons = <>
+        <NavLink
+            className={styles.NavLink}
+            activeClassName={styles.Active}
+            to="/supported"
+        >
+            <i className="fa-solid fa-heart"></i>Supported
+        </NavLink>
+        <NavLink
+            className={styles.NavLink}
+            activeClassName={styles.Active}
+            to="/liked"
+        >
+            <i className="fa-regular fa-futbol"></i>Liked
+        </NavLink>
+        <NavLink
+            className={styles.NavLink}
+            to={`/profiles/${currentUser?.profile_id}`}
+        >
+            <Avatar src={currentUser?.profile_image} text="Profile" height={40} />
+        </NavLink>
+        <NavLink
+            className={styles.NavLink}
+            to="/"
+            onClick={handleSignOut}
+        >
+            <i className="fas fa-sign-out-alt"></i>Sign Out
+        </NavLink>
+    </>;
+
+    const loggedOutIcons = (
+        <>
+            <NavLink
+                className={styles.NavLink}
+                activeClassName={styles.Active}
+                to="/signin"
+            >
+                <i className="fas fa-sign-in-alt"></i>Sign in
+            </NavLink>
+            <NavLink
+                className={styles.NavLink}
+                activeClassName={styles.Active}
+                to="/signup"
+            >
+                <i className="fas fa-user-plus"></i>Sign up
+            </NavLink>
+        </>
+    );
+
+    return (
+        <Navbar expanded={expanded} className={styles.NavBar} expand="md" fixed="top">
+            <Container>
+                <NavLink to="/">
+                    <Navbar.Brand>
+                        <img src={logo} alt="logo" height="30" />
+                    </Navbar.Brand>
+                </NavLink>
+                {currentUser && addPostIcon}
+                <Navbar.Toggle
+                    ref={ref}
+                    onClick={() => setExpanded(!expanded)}
+                    aria-controls="basic-navbar-nav"
+                />
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="ml-auto text-left">
+                        <NavLink
+                            exact
+                            className={styles.NavLink}
+                            activeClassName={styles.Active}
+                            to="/">
+                            <i className="fa-solid fa-house"></i>Home
+                        </NavLink>
+                        {currentUser ? loggedInIcons : loggedOutIcons}
+                    </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
+    );
+};
+
+export default NavBar;
---
+++ frontend/src/pages/profiles/PopularProfiles.js
@@ -0,0 +1,61 @@
+import React, { useEffect } from 'react';
+import { Container } from 'react-bootstrap';
+import appStyles from '../../App.modules.css'
+import { axiosReq } from '../../api/axiosDefaults';
+import Asset from '../../components/Asset';
+import { useCurrentUser } from '../../contexts/CurrentUserContext';
+import Profile from './Profile';
+import { useProfileData } from '../../contexts/ProfileDataContext';
+
+
+const PopularProfiles = ({ mobile }) => {
+    const [profileData, setProfileData] = useState({
+        pageProfile: { results: [] },
+        popularProfiles: { results: [] },
+    });
+    const { popularProfiles } = useProfileData();
+    const currentUser = useCurrentUser();
+
+    useEffect(() => {
+        const handleMount = async () => {
+            try {
+                const { data } = await axiosReq.get(
+                    "/profiles/?ordering=-followers_count"
+                );
+                setProfileData(prevState => ({
+                    ...prevState,
+                    popularProfiles: data,
+                }));
+            } catch(err) {
+                console.log(err);
+            }
+        };
+
+        handleMount();
+    }, [currentUser]);
+
+    return (
+        <Container className={`${appStyles.Content} ${mobile && 'd-lg-none text-center mb-3'}`}>
+            {popularProfiles.results.length ? (
+                <>
+                    <p>Most followed profiles.</p>
+                    {mobile ? (
+                        <div className="d-flex justify-content-around">
+                          {popularProfiles.results.slice(0,4).map((profile) => (
+                            <Profile key={profile.id} profile={profile} mobile />
+                          ))}  
+                        </div>
+                    ) : (
+                        popularProfiles.results.map((profile) => (
+                            <Profile key={profile.id} profile={profile} />
+                        ))
+                    )}
+                </>
+            ) : (
+                <Asset spinner />
+            )}
+        </Container>  
+    );
+};
+
+export default PopularProfiles;
