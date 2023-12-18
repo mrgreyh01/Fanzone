472d4d8796bd78ce8ef1728d3e1eb234b5ea56e8
WelshDan <>
2023-12-18 13:33:55 +0000

    error: fixing deployment error

--- drf_fanzone/settings.py
+++ drf_fanzone/settings.py
@@ -62,10 +62,8 @@ ALLOWED_HOSTS = [
     'localhost',
     'drf-fanzone.herokuapp.com',
     '8000-welshdan-fanzone-5jjix6r3t6.us2.codeanyapp.com',
-    'https://drf-fanzone-5jjix6r3t6.us2.herokuapp.com',
+    'https://drf-fanzone-dfbf06e05b90.herokuapp.com',
 ]
-
-
 
 # Application definition
 
