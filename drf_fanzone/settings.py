89df596efd058036daa7a22a6e8d965386ed3760
WelshDan <dan.roberts78@gmail.com>
2024-02-13 10:58:01 +0000

    corrected issues with cors and log in

--- drf_fanzone/settings.py
+++ drf_fanzone/settings.py
@@ -105,9 +105,10 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-CORS_ALLOWED_ORIGINS = [
-    os.environ.get('CLIENT_ORIGIN')
-]
+if 'CLIENT_ORIGIN' in os.environ: 
+    CORS_ALLOWED_ORIGINS = [
+        os.environ.get('CLIENT_ORIGIN')
+    ]
 
 CORS_ALLOW_CREDENTIALS = True
 
